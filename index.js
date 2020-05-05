90bf950229d222dd24156122e0a914f217325df5
amiraITI2019 <amirareda.iti.intake40@gmail.com>
2020-05-06 01:03:23 +0200

    hgj

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body.weather);
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
 //end weather
