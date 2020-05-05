cc8a96b4da5a201a7a6a5ea26e65342664a8fb51
amiraITI2019 <amirareda.iti.intake40@gmail.com>
2020-05-06 00:04:30 +0200

    test

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function callSendAPI(sender_psid, response) {
     },
     "message": response
   };
- 
+  const request = require('request');
+
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
@@ -31,7 +32,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!',PAGE_ACCESS_TOKEN )
+      console.log('message sent!');
     } else {
       console.error("Unable to send message:" + err);
     }
