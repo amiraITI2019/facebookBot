c332f9c04186c35754e85acb34c42bc0c471dbc9
amiraITI2019 <amirareda.iti.intake40@gmail.com>
2020-05-05 23:13:39 +0200

    call send api request library function

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.post('/webhook', (req, res) => {
   if (webhook_event.message) {
     handleMessage(sender_psid, webhook_event.message);        
   }
-  //  else if (webhook_event.postback) {
-  //   handlePostback(sender_psid, webhook_event.postback);
-  // }
+   else if (webhook_event.postback) {
+    handlePostback(sender_psid, webhook_event.postback);
+  }
     });
   
     // Return a '200 OK' response to all events
@@ -124,4 +124,21 @@ app.get('/webhook', (req, res) => {
         console.error("Unable to send message:" + err);
       }
     }); 
+  }
+
+
+  function handlePostback(sender_psid, received_postback) {
+    let response;
+    
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+  
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
   }
